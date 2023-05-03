038c6557a223473543e279020ab68e93965a8aee
rana <rana.ahammed.012@gmail.com>
2023-05-03 19:27:25 +0600

    .

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -14,16 +14,16 @@ const Landing = ({ setSelectedPage }) => {
 			{/* IMAGE SECTION */}
 			<div className="md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
 				{isAboveMediumScreens ? (
-					<div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
+					<div className="relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:w-full before:max-w-[400px] before:h-full before:border-2 before:border-blue before:z-[-1]">
 						<img
-							className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[200px] md:max-w-[400px]"
-							src="../assets/profile-image.png"
+							className="hover:filter transition duration-500 z-10 w-500px h-400px max-w-[200px] md:max-w-[400px]"
+							src="../assets/profile-image.jpeg"
 							alt="profile"
 						/>
 					</div>
 				) : (
 					<img
-						className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
+						className="hover:filter hover:saturate-200 transition duration-500 z-10 w-500px h-400px max-w-[400px] md:max-w-[600px]"
 						src="../assets/profile-image.png"
 						alt="profile"
 					/>
